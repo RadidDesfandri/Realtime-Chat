e8e9968dd0657b556bc36ecc293efcce3a349043
RadidDesafandri <radietdesfandri@gmail.com>
2024-11-02 07:15:35 +0700

    setup project

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div className="bg-light h-full">
+        <p className="text-dark text-3xl">Hello chat index</p>
+      </div>
+    );
+  }
+  
